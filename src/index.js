238bb66702dcd2d95415d7ac6b6f3a672798fb76
Mykhailo Shyhin <johnglagol@gmail.com>
2022-06-26 08:23:54 +0300

    Fix index.js
    Оптимізував index.js

--- src/index.js
+++ src/index.js
@@ -1,32 +1,16 @@
-const btnBurgerMenu = document.querySelector('.menu-button__open');
-const headerMenu = document.querySelector('.header__menu');
-const btnCloseMenu = document.querySelector('.menu-button__close');
-const nav1CloseMenu = document.querySelector('.js1');
-const nav2CloseMenu = document.querySelector('.js2');
-const nav3CloseMenu = document.querySelector('.js3');
-const nav4CloseMenu = document.querySelector('.js4');
-const nav5CloseMenu = document.querySelector('.js5');
-    
+const btnBurgerMenu = document.querySelector('.js-menu-button__open');
+const headerMenu = document.querySelector('.js-header__menu');
+const btnCloseMenu = document.querySelector('.js-menu-button__close');
+const navLinkMenu = document.querySelectorAll('.js-nav__link');
+
 btnBurgerMenu.addEventListener('click', () => {
-    headerMenu.classList.toggle('is-open')
+    headerMenu.classList.toggle('is-open');
 });
 
 btnCloseMenu.addEventListener('click', () => {
-    headerMenu.classList.toggle('is-open')
+    headerMenu.classList.toggle('is-open');
 });
 
-nav1CloseMenu.addEventListener('click', () => {
-    headerMenu.classList.toggle('is-open')
-});
-nav2CloseMenu.addEventListener('click', () => {
-    headerMenu.classList.toggle('is-open')
-});
-nav3CloseMenu.addEventListener('click', () => {
-    headerMenu.classList.toggle('is-open')
-});
-nav4CloseMenu.addEventListener('click', () => {
-    headerMenu.classList.toggle('is-open')
-});
-nav5CloseMenu.addEventListener('click', () => {
-    headerMenu.classList.toggle('is-open')
-});
+navLinkMenu.forEach(el =>
+    el.addEventListener('click', () =>
+        headerMenu.classList.toggle('is-open')));
