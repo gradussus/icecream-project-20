46e7f34a81a2baa435e7b37343ae79e4551df018
gradussus <103496948+gradussus@users.noreply.github.com>
2022-06-28 12:35:42 +0000

    Deploying to gh-pages from @ gradussus/icecream-project-20@d9275363c67a4ce781f0a8bfd1ee68079d0a3fe1 🚀

---
+++ index.997368cb.js
@@ -0,0 +1,2 @@
+function changeHeaderBackground(){var e=document.getElementById("header"),n=e.offsetTop;window.pageYOffset>n?e.classList.add("js-no-transparency"):e.classList.remove("js-no-transparency")}window.onscroll=function(){return changeHeaderBackground()};
+//# sourceMappingURL=index.997368cb.js.map
