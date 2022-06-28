b8e2138495bdf2a27bd17add4ab0f09bb9eb2a14
gradussus <103496948+gradussus@users.noreply.github.com>
2022-06-28 04:25:01 +0000

    Deploying to gh-pages from @ gradussus/icecream-project-20@331fc28fbc2d5d7bb7b3fa33a97916aaeb0a5c1c 🚀

---
+++ index.83ca1b96.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[contacts-modal-open]"),closeModalBtn:document.querySelector("[contacts-modal-close]"),modal:document.querySelector("[contacts-modal]")};function e(){document.body.classList.toggle("contacts-modal-open"),o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.83ca1b96.js.map
