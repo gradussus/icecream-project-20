d813f3c08492d1200f79990d20efc2149dc5c6e0
gradussus <103496948+gradussus@users.noreply.github.com>
2022-06-29 04:54:13 +0000

    Deploying to gh-pages from @ gradussus/icecream-project-20@e9772e288b59c5953aa350fc9554d08147fa91a8 🚀

---
+++ index.8db97894.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".mySwiper",{slidesPerView:1,effect:"creative",creativeEffect:{prev:{scale:0},next:{translate:["100%",0,0]}},keyboard:{enabled:!0,onlyInViewport:!1,pageUpDown:!0},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0,dynamicBullets:!0},loop:!0,speed:1500,autoplay:{delay:2e3}});
+//# sourceMappingURL=index.8db97894.js.map
--- index.df668cf7.js
+++
@@ -1,2 +0,0 @@
-var swiper=new Swiper(".mySwiper",{slidesPerView:1,effect:"creative",creativeEffect:{prev:{scale:0},next:{translate:["100%",0,0]}},keyboard:{enabled:!0,onlyInViewport:!1,pageUpDown:!0},mousewheel:{invert:!0},pagination:{el:".swiper-pagination",type:"bullets",clickable:!0},loop:!0,speed:1500,autoplay:{delay:3500}});
-//# sourceMappingURL=index.df668cf7.js.map
